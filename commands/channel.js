d95ebf2974e0654677329a263085979e3246ea3e
Kamzys <aresta.putra.wardhani.n@gmail.com>
2026-03-26 11:55:01 +0700

    teks channel update

--- commands/channel.js
+++ commands/channel.js
@@ -1,4 +1,9 @@
 module.exports = async (m) => {
-const text = `Follow Zy's Development channel: https://whatsapp.com/channel/0029VbC4CUoGk1FxB6Gh7a0P`
+const text = `📢 Zy’s Dev Channel
+
+Update bot, sneak peek fitur baru, dan pengumuman event ada disini.
+
+Join:
+https://whatsapp.com/channel/0029VbC4CUoGk1FxB6Gh7a0P`
 m.reply(text)
 }
